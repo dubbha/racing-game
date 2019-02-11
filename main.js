c6142a90396b19a464d7392135886f0d7fbd1f2d
Olena Shynkarenko <olena_shynkarenko@epam.com>
2019-02-11 12:27:01 +0200

    obstacles random generation added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+let lastTimeObstacleCreated = performance.now();
+let minPauseBetweenObstacles = 2000;
 var canvas = document.getElementById('play-field');
 var ctx = canvas.getContext('2d');
 
@@ -13,25 +14,39 @@ img.onload = function() {
 	ctx.drawImage(img, 80, 80)
 }
 
-function draw() {
+function draw(timestamp) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //drawCar();
   drawSquare();
+	if (timestamp - lastTimeObstacleCreated > minPauseBetweenObstacles) {
+		generateObstacle();
+    lastTimeObstacleCreated = timestamp;
+	}
   requestAnimationFrame(draw);
 }
 
-let brickX = 0;
+let brickX;
 let brickY = 0;
-let brickWidth  = 50;
-let brickHeight = 50;
-let increment = 3;
+let brickWidth;
+const brickHeight = 50;
+const increment = 3;
 
 function drawSquare() {
-  ctx.beginPath();
+	ctx.beginPath();
   ctx.rect(brickX, brickY, brickWidth, brickHeight);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
   brickY += increment;
+
 }
+
+function generateObstacle() {
+	const minWidth = canvas.width * 0.1;
+  const maxWidth = canvas.width * 0.3;
+	brickWidth =  minWidth + (maxWidth - minWidth) * Math.random();
+  brickX = (canvas.width - brickWidth) * Math.random();
+}
+generateObstacle();
 
 draw();
