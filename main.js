f0ff59c5e28553e97c90ff37a9917874cf69d8dd
Oksana Kravchuk <oksana_kravchuk1@epam.com>
2019-02-11 12:03:31 +0200

    add content

--- main.js
+++ main.js
@@ -2,16 +2,15 @@
 var canvas = document.getElementById('play-field');
 var ctx = canvas.getContext('2d');
 
-canvas.width = 1300;
-canvas.height = 1000;
+canvas.width = 800;
+canvas.height = 1200;
 
 var img = new Image();
 
-img.src = 'iconfinder_BT_c3top_905662.png';
+img.src = 'car.png';
 
 img.onload = function() {
 	ctx.drawImage(img, 80, 80)
 }
-
 
 
