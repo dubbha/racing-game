69d85165bc163523d9238a72b4d25bad38924271
Oksana Kravchuk <oksana_kravchuk1@epam.com>
2019-02-11 12:21:07 +0200

    edits

--- main.js
+++ main.js
@@ -3,15 +3,11 @@ var canvas = document.getElementById('play-field');
 var ctx = canvas.getContext('2d');
 
 canvas.width = 800;
-canvas.height = 1200;
+canvas.height = 650;
 
 var img = new Image();
 
 img.src = 'car.png';
-
-img.onload = function() {
-	ctx.drawImage(img, 80, 80)
-}
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -19,19 +15,17 @@ function draw() {
   requestAnimationFrame(draw);
 }
 
-let brickX = 0;
-let brickY = 0;
+let brickX = 370;
+let brickY = 570;
 let brickWidth  = 50;
 let brickHeight = 50;
 let increment = 3;
 
 function drawSquare() {
   ctx.beginPath();
-  ctx.rect(brickX, brickY, brickWidth, brickHeight);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
+  ctx.drawImage(img, brickX, brickY);
   ctx.closePath();
-  brickY += increment;
+  brickY -= increment;
 }
 
 draw();
